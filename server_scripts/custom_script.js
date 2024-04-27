ef46d06450f8aff729fba309ff54a4bd7f9950d7
TheTobicraft11s <167756632+TheTobicraft11s@users.noreply.github.com>
2024-04-27 17:15:50 +0200

    Delete server_scripts directory

--- server_scripts/custom_script.js
+++
@@ -1,8 +0,0 @@
-ServerEvents.loaded, function (event) {
-    let playername = event.player.name.toString();
-    let cleanPlayername = playername.slice(8, -1);
-    let command = "give " + cleanPlayername + " createcoins:coopercoin";
-    event.server.schedule(30 * MINUTE, event.server, function (callback) {
-      callback.data.runCommand(command)
-    })
-}
--- server_scripts/on_serverstart.js
+++
@@ -1,7 +0,0 @@
-LevelEvents.loaded((event) => {
-    event.server.runCommandSilent(`say @a Server ist Online`);
-    event.server.runCommandSilent(`gamerule doFireTick false`);
-    event.server.runCommandSilent(`gamerule playersSleepingPercentage 50`); //Stellt ein das nur die hälfte der spieler schlafen muss das die nacht geskippt wird
-  });
-
-  
